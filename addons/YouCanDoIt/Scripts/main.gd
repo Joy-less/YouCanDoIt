63c021d6f5e4c751e092aceaa36dad0cea602a54
Joyless <65855333+Joy-less@users.noreply.github.com>
2024-08-21 13:36:43 +0100

    Add sound effects

--- addons/YouCanDoIt/Scripts/main.gd
+++ addons/YouCanDoIt/Scripts/main.gd
@@ -6,12 +6,13 @@ extends EditorPlugin
 @export var duration_seconds:float = 5
 @export var transition_seconds:float = 1
 @export var transition_distance:float = 540
+@export var volume_db:float = -10
 
 var overlay_dock:Control = load(addon_path.path_join("Scenes/OverlayDock.tscn")).instantiate()
 var messages:Dictionary = JSON.parse_string(FileAccess.get_file_as_string(addon_path.path_join("Text/Messages.json")))
 var timer_seconds:float = 0
 
-const addon_path:String = "res://addons/KeepWorkingHard"
+const addon_path:String = "res://addons/YouCanDoIt"
 
 func _enter_tree()->void:
 	reset_timer()
@@ -41,6 +42,12 @@ func _process(delta:float)->void:
 	# Transition overlay in
 	await transition_overlay(true)
 	
+	# Play sound
+	var audio_player:AudioStreamPlayer = overlay_dock.get_node("AudioPlayer")
+	audio_player.stream = random_sound()
+	audio_player.volume_db = volume_db
+	audio_player.play()
+	
 	# Wait duration
 	await get_tree().create_timer(duration_seconds).timeout
 	
@@ -62,18 +69,15 @@ func random_message(type:String)->String:
 
 func random_girl(type:String)->Texture2D:
 	var girl_directory:String = addon_path.path_join("Images/Girls").path_join(type)
-	
-	# Get all girls from import files
-	var girl_paths:PackedStringArray = []
-	for girl_path:String in DirAccess.get_files_at(girl_directory):
-		if girl_path.ends_with(".import"):
-			girl_paths.append(girl_path.trim_suffix(".import"))
-	
-	# Load random girl
-	var girl_path:String = girl_paths[randi_range(0, girl_paths.size() - 1)]
-	return load(girl_directory.path_join(girl_path))
+	var girl_paths:Array = get_files_at(girl_directory)
+	return load(girl_directory.path_join(girl_paths.pick_random()))
 
-func transition_overlay(to_visible:bool):
+func random_sound()->AudioStream:
+	var sound_directory:String = addon_path.path_join("Sounds")
+	var sound_paths:Array = get_files_at(sound_directory)
+	return load(sound_directory.path_join(sound_paths.pick_random()))
+
+func transition_overlay(to_visible:bool)->void:
 	var background:Control = overlay_dock.get_node("Background")
 	var transition:Tween = get_tree().create_tween()
 	
@@ -85,3 +89,10 @@ func transition_overlay(to_visible:bool):
 		transition.tween_property(background, "position:y", transition_distance, transition_seconds)
 	
 	await transition.finished
+
+func get_files_at(directory:String)->Array:
+	var files:Array = []
+	for file:String in DirAccess.get_files_at(directory):
+		if file.ends_with(".import"):
+			files.append(file.trim_suffix(".import"))
+	return files
